56ad46f96adee3c5914d4f170f6e65bf6c960a34
Stewart Lynch <slynch@createchsol.com>
2022-03-02 11:39:10 -0800

    Initial Commit

---
+++ Realm ToDo-1/Realm_ToDo_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_ToDo_1App.swift
+//  Realm ToDo-1
+//
+//  Created by Stewart Lynch on 2022-03-02.
+//
+
+import SwiftUI
+
+@main
+struct Realm_ToDo_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
