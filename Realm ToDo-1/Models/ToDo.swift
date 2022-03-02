b72fe1169d80048f8452a22bb0149cbd769f7061
Stewart Lynch <slynch@createchsol.com>
2022-03-02 14:48:24 -0800

    Added Model

---
+++ Realm ToDo-1/Models/ToDo.swift
@@ -0,0 +1,36 @@
+//
+//  ToDo.swift
+//  Realm ToDo-1
+//
+//  Created by Stewart Lynch on 2022-03-02.
+//
+
+import Foundation
+import RealmSwift
+
+class ToDo: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var name: String
+    @Persisted var completed = false
+    @Persisted var urgency: Urgency = .neutral
+    
+    enum Urgency: Int, PersistableEnum {
+        case trivial, neutral, urgent
+        
+        var text: String {
+            switch self {
+            case .trivial:
+                return "Trivial"
+            case .neutral:
+                return "Neutral"
+            case .urgent:
+                return "Urgent"
+            }
+        }
+    }
+    
+    convenience init(name: String) {
+        self.init()
+        self.name = name
+    }
+}
