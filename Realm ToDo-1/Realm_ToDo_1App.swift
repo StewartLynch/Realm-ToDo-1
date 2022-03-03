32da9bff40b41d941c9762b26c85c061abeccf31
Stewart Lynch <slynch@createchsol.com>
2022-03-02 18:58:41 -0800

    Added onAppear

--- Realm ToDo-1/Realm_ToDo_1App.swift
+++ Realm ToDo-1/Realm_ToDo_1App.swift
@@ -12,6 +12,10 @@ struct Realm_ToDo_1App: App {
     var body: some Scene {
         WindowGroup {
             ToDoListView()
+                .onAppear {
+                    print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path)
+                     UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+                }
         }
     }
 }
